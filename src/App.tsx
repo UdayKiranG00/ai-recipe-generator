a0637b530f7c9141840e2f3319b1c5bf3787b835
UdayKiranG00 <udayganguru2000@gmail.com>
2025-04-23 10:45:39 -0400

    professional to personal

--- src/App.tsx
+++ src/App.tsx
@@ -48,7 +48,7 @@ function App() {
         <div className="app-container">
             <div className="header-container">
                 <h1 className="main-header">
-                    Meet Your Professional
+                    Meet Your Personal
                     <br />
                     <span className="highlight">Recipe AI Generate</span>
                 </h1>
